39a8c852e70c5e79d95bb72262085f853a330752
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-04 10:30:03 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
-
-  const find=()=>{
-    fetch('http://localhost:5050/api/product')
+const[name,setname]=useState([])
+  const find= async(e)=>{
+    e.preventDefault()
+    const wait= await fetch('http://localhost:5050/api/product')
     .then((res)=>res.json())
     .then((data)=>{
       console.log(data)
+      setname(...name,data.message)
+alert()
     })
   }
-
+const search= async()=>{
+  alert()
+   const wait= await fetch('http://localhost:5050/api/product')
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data)
+      setname(...name,data.message)
+alert()
+    })
+}
   return (
     <div className="App">
-  <button onClick={find}>click</button>
+      <form onSubmit={find}>
+
+        <input type='submit'  />
+      </form>
+
+      {name}
+  <button onClick={search}>click</button>
     </div>
   );
 }
